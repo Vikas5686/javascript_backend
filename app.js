9b7a2a21c2f3358017aa7ce5414fc3d553350819
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-04-05 15:05:33 +0530

    basic server

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 require("dotenv").config();
 const app = express()
-const cors = require("cors")
 
 
 const port = process.env.PORT || 3000;
 
-app.use(cors())
 app.use(express.json())
 
 app.listen(port, () => {
