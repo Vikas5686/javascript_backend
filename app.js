13142d40063f3852820b4f29d9a9d893d48f3c6f
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-04-05 14:19:34 +0530

    changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require("./db/connection")
 const users = require("./models/userSchema")
 const router =require("./routes/router")
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
