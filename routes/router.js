f412c1e1ae53634dfbdd2ec4df739ccff886c91f
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-04-07 00:14:40 +0530

    almost done

--- routes/router.js
+++ routes/router.js
@@ -32,7 +32,15 @@ router.post("/register", async (req, res) => {
 
 router.get("/getrequist", async (req, res) => {
     try {
-        const user = await users.find();
+        const user = await users.find().sort({"Score" : -1});
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+router.get("/delete", async (req, res) => {
+    try {
+        const user = await users.deleteMany();
         res.status(201).json(user)
     } catch (error) {
         res.status(404).json(error)
@@ -43,9 +51,21 @@ router.get("/getUser/:id", async (req, res) => {
     try {
         console.log(req.params)
         const { id } = req.params
-        const userindividual = await users.findById({ _id: id })
+        const userindividual = await users.find({ email: id })
         console.log(userindividual)
         res.status(201).json(userindividual)
+    } catch (error) {
+        console.log("djflsfj")
+        res.status(404).json(error)
+    }
+})
+router.patch("/Update/:id", async (req, res) => {
+    try {
+        console.log(req.body)
+       const { id } = req.params
+        const userindividual = await users.findOneAndUpdate({ email: id }, {$set:{Score: req.body.Score}})
+       await console.log(userindividual)
+       await res.status(201).json(userindividual)
     } catch (error) {
         res.status(404).json(error)
     }
